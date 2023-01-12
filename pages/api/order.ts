6a51d4614b098a93f6bb42089672e1deca4ffc15
javier torales <jtorales2016@gmail.com>
2023-01-12 03:57:35 -0300

    la funcion que hacia order paso al controller

--- pages/api/order.ts
+++ pages/api/order.ts
@@ -1,41 +1,11 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
 import methods from 'micro-method-router'
-import { comrpuebaToken } from "./lib/compruebaToken/index"
-import { User } from "../../models/user"
-import { Product } from '../../models/products'
-import { Order } from '../../models/order'
-import { createPreference } from './lib/mercadoPagoFunctions'
+import { intencionDeCompra } from '../../controllers/orderControllers'
 export default methods({
   async post(req: NextApiRequest, res: NextApiResponse) {
-    const result = comrpuebaToken(req, res);
-    const user = new User(result.userId);
-    const producId = JSON.parse(req.query.productId as string);
-    const product = await Product.findProduct(producId);
-    if (product != null) {
-      const order = await Order.createOrder(user.id, product.productId);
-      const prefences = await createPreference({
-        "items": [
-          {
-            "title": `${product.name}`,
-            "description": "iphone x",
-            "picture_url": "http://www.myapp.com/myimage.jpg",
-            "category_id": "car_electronics",
-            "quantity": 1,
-            "currency_id": "ARS",
-            "unit_price": product.price
-          }
-        ],
-        "back_urls": {
-          "sucess": "https://mypageEcommerce"
-        },
-        "external_reference": `${order.orderId}`,
-        "notification_url": "https://dw9-m5.vercel.app/api/webHooks/mercadopago"
-      });
-      res.send({ linkDePago: prefences.init_point });
-    } else {
-      res.send({ erorr: "no hay producto con ese id" })
-    }
-
+    const intCompra = await intencionDeCompra(req);
+    if(intCompra.error)res.send({error:intCompra.error})
+    res.send(intCompra);
   }
 
 })
