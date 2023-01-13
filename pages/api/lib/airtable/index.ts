98a53ca1e1fcce1012338898a8b0d2826a3ef39e
javier torales <jtorales2016@gmail.com>
2023-01-13 09:52:30 -0300

    se añadio la coneccion a airtable

---
+++ pages/api/lib/airtable/index.ts
@@ -0,0 +1,2 @@
+import Airtable from 'airtable';
+export var base = new Airtable({apiKey: 'keyonRD74cxJ0MZaE'}).base('app9SIwlKDVRS6cZc');
--- pages/api/me/index.ts
+++ pages/api/me/index.ts
@@ -2,19 +2,20 @@ import type { NextApiRequest, NextApiResponse } from 'next'
 import { meData, actualizaMeData } from '../../../controllers/userControllers';
 import methods from 'micro-method-router'
 import * as yup from 'yup';
+
 let schema = yup.object().shape({
-    nombre: yup.string(),
+    nombre: yup.string().required(),
 }).noUnknown(true).strict();
+
 export default methods({
     async get(req: NextApiRequest, res: NextApiResponse) {
         const resp = await meData(req);
-        res.send({ resp, method: "get" });
+        res.send(resp);
     },
     async patch(req: NextApiRequest, res: NextApiResponse) {
-        
         try {
             await schema.validate(req.body);
-            const resp = await actualizaMeData(req,req.body);
+            const resp = await actualizaMeData(req, req.body);
             res.send(resp)
         } catch (err) {
             res.status(400).send({ message: err })
