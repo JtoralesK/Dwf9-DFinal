43937765b7ad492cd59d3b197e58fa5f58a6a73c
javier torales <jtorales2016@gmail.com>
2023-01-13 09:54:02 -0300

    se añadio coneccion a algolia

---
+++ pages/api/lib/algolia/index.ts
@@ -0,0 +1,4 @@
+import algoliasearch from "algoliasearch"
+const client = algoliasearch('FKMA1XLJS1', 'de075451ff9b81c93aa1c8e311248b7f') ;
+export  const index = client.initIndex('test_index');
+  
