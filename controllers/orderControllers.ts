772d963f7bd2056140a47fdbc4e414e4fff3ed8b
javier torales <jtorales2016@gmail.com>
2023-01-14 09:58:40 -0300

    se actualizo link de notification_url

--- controllers/orderControllers.ts
+++ controllers/orderControllers.ts
@@ -22,7 +22,7 @@ const preference = (product, order) => {
             "sucess": "https://mypageEcommerce"
         },
         "external_reference": `${order.orderId}`,
-        "notification_url": "https://dw9-m5.vercel.app/api/ipn/mercadopago"
+        "notification_url": "https://dwf9-d-final.vercel.app/api/ipn/mercadopago"
     }
 }
 
