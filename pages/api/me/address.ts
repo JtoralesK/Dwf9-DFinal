c1dbafabfc20406eb647a0952c8d885838a004a1
javier torales <jtorales2016@gmail.com>
2023-01-16 07:50:09 -0300

    se agrego nuevo endpoint

---
+++ pages/api/me/address.ts
@@ -0,0 +1,21 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+import { meData, actualizaMeData } from '../../../controllers/userControllers';
+import methods from 'micro-method-router'
+import * as yup from 'yup';
+
+let schema = yup.object().shape({
+  nombre: yup.string().required(),
+}).noUnknown(true).strict();
+
+export default methods({
+  async get(req: NextApiRequest, res: NextApiResponse) {
+    try {
+      await schema.validate(req.body);
+      const resp = await actualizaMeData(req, req.body);
+      res.send(resp)
+    } catch (err) {
+      res.status(400).send({ message: err })
+    }
+  }
+})
+
--- pages/api/products/[id].ts
+++ pages/api/products/[id].ts
@@ -1,11 +1,17 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
 import methods from 'micro-method-router'
 import { getProduct } from '../../../controllers/productControllers';
+import * as yup from 'yup';
+
+let schema = yup.object().shape({
+    id: yup.string().required(),
+}).noUnknown(true).strict();
 
 export default methods({
   async get(req: NextApiRequest, res: NextApiResponse) {
-    const id = req.query.id as string;
-    const product = await getProduct(id);        
-    res.send(product);
+        const id = req.query.id as string;
+      const product = await getProduct(id);        
+      res.send(product);    
+      
 }
 })
