13a1c10355444e3720c90374b6bb3a7f663f3e7d
javier torales <jtorales2016@gmail.com>
2023-02-05 20:18:09 -0300

    incorrect import fix

--- pages/api/auth/index.ts
+++ pages/api/auth/index.ts
@@ -1,6 +1,6 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import { sendCode } from "controllers/authControllers";
-import { sendEmail } from "lib/sendgrid";
+import { sendEmail } from "lib/nodemailer";
 import methods from "micro-method-router";
 import { handlerCORS } from "externalFunctions/handlerCors";
 import * as yup from "yup";
